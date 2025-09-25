5c8d4c7f4e9ba54077edba3b23f76bf6d1b880e6
MlikoKakao <mattewczlp@seznam.cz>
2025-09-25 14:14:36 +0200

    starting cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <cmath>
+
+
 using namespace std;
 
+
+static double calculator(){
+	int a,b;
+	int result;
+	cout << &result;
+	cout << "First number: ";
+	cin >> a;
+	cout << "Second number: ";
+	cin >> b;
+
+	return result;
+}
 int main() {
-	cout << "Hello world" << endl;
+	int x = 5;
+	string first;
+	cout << "First action: ";
+	getline(cin, first);
+	cout << "Your string: "<< first << '\n';
+
+	cout << "Hello world " << x << endl;
+	calculator();
+
+
 	return 0;
+
 }
